05833bdfbf279d5fcf52db8366459e27bfc1ceb2
Roman Chaban <roman.chaban1001@gmail.com>
2024-04-20 17:56:57 +0300

    Add forEach prototype method

--- js/forEach/forEach.js
+++ js/forEach/forEach.js
@@ -1,1 +1,37 @@
 'use strict';
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numbers[20] = 20;
+
+Array.prototype.forEachTwo = function (callback, thisArg) {
+  let obj = Object(this);
+  let len = obj.length;
+  let iter = 0;
+  let context = this;
+
+  console.log(obj);
+
+  if (this == null) {
+    throw new Error("Can't iterate over undefined or null");
+  }
+
+  if (typeof callback !== 'function') {
+    throw new Error('callback is not a function');
+  }
+
+  if (arguments.length > 1) {
+    context = thisArg;
+  }
+
+  while (iter < len) {
+    if (iter in obj) {
+      callback.call(context, this[iter], iter, obj);
+    }
+    iter++;
+  }
+};
+
+numbers.forEachTwo((item, index, array) => {
+  console.log({ item, index });
+});
